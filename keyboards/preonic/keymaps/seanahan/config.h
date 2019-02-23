42af49d2cda83fc2ec901229045d8c7cbf94865f
Sean Monahan <1907654+seancmonahan@users.noreply.github.com>
2019-02-23 14:53:45 -0500

    LEDs working with rbg_matrix off of arm_rgb branch... will try to see if it works against master too

---
+++ keyboards/preonic/keymaps/seanahan/config.h
@@ -0,0 +1,54 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "config_common.h"
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(COLEMAK_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }
+; //FUCKING SUBLIME TEXT WANTS TO INDENT EVERYTHING IF THIS SEMICOLON ISN'T HERE
+#endif
+
+#define MUSIC_MASK (keycode != KC_NO)
+
+/*
+ * MIDI options
+ */
+
+/* Prevent use of disabled MIDI features in the keymap */
+//#define MIDI_ENABLE_STRICT 1
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
+#define MIDI_BASIC
+
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+/*
+#ifdef RGB_MATRIX_ENABLE
+#define RGBLED_NUM 9
+#define RGB_DI_PIN A1
+#define DRIVER_LED_TOTAL 9
+#define RGB_MATRIX_KEYPRESSES
+#define RGBLIGHT_ANIMATIONS
+#endif
+*/
+
+
+#endif
